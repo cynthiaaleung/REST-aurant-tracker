159b9ffd95d6c0ab63280f99b4994f9fa5df78dd
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-02-12 15:32:04 -0800

    amend restaurant detail page to account for reviews, pass reviews as props, replace mock data with actual values, add in missing key props

--- client/src/components/RestaurantDetailPage.jsx
+++ client/src/components/RestaurantDetailPage.jsx
@@ -4,7 +4,6 @@ import restaurantFinder from "../apis/restaurantFinder";
 import { RestaurantsContext } from "../context/restaurantsContext";
 import AddReview from "./AddReview";
 import Reviews from "./Reviews";
-import StarRating from "./StarRating";
 
 export default function RestaurantDetailPage() {
   const { id } = useParams();
@@ -14,7 +13,7 @@ export default function RestaurantDetailPage() {
     const fetchData = async () => {
       try {
         const response = await restaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurant)
+        setSelectedRestaurant(response.data.data);
       } catch (err) {}
     }
     fetchData();
@@ -25,7 +24,7 @@ export default function RestaurantDetailPage() {
       {selectedRestaurant && (
         <>
           <div className="mt-3">
-            <Reviews/>
+            <Reviews reviews={selectedRestaurant.reviews}/>
             <AddReview/>
           </div>
         </>
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,17 +1,27 @@
 import StarRating from "./StarRating";
 
-export default function Reviews() {
+export default function Reviews({ reviews }) {
   return (
     <div className="row row-cols-3 mb-2">
-      <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
-        <div className="card-header d-flex justify-content-between">
-          <span>Cynthia</span>
-          <span><StarRating rating={3}/></span>
+      {reviews.map((review) => {
+        return(
+          <div 
+            key ={review.id}
+            className="card text-white bg-primary mb-3 mr-4" 
+            style={{maxWidth: "30%"}}
+          >
+          <div className="card-header d-flex justify-content-between">
+            <span>{review.name}</span>
+            <span>
+              <StarRating rating={review.rating}/>
+            </span>
+          </div>
+          <div className="card-body">
+            <p className="card-text">{review.review}</p>
+          </div>
         </div>
-        <div className="card-body">
-          <p className="card-text">Awesome restaurant</p>
-        </div>
-      </div>
+        );
+      })}
     </div>
   )
 }
