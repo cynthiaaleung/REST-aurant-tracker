d121373a38d49a164a9ada49e084e1428f737dc0
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2022-02-12 15:16:38 -0800

    create add review form and states

--- client/src/components/RestaurantDetailPage.jsx
+++ client/src/components/RestaurantDetailPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import restaurantFinder from "../apis/restaurantFinder";
 import { RestaurantsContext } from "../context/restaurantsContext";
+import AddReview from "./AddReview";
 import Reviews from "./Reviews";
 import StarRating from "./StarRating";
 
@@ -25,6 +26,7 @@ export default function RestaurantDetailPage() {
         <>
           <div className="mt-3">
             <Reviews/>
+            <AddReview/>
           </div>
         </>
       )}
